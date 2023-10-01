0af29f8d555a6dccdbf8fb1dc55c427dc705f952
Evans Roy <evansroysir@gmail.com>
2023-10-01 21:10:32 +0300

    Initial Comit with laravel migrations and front-end Auth UI

---
+++ front-end/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<script>
+ export default {
+    name:"Login"
+ }
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold ">
+        Dashboard
+    </h1>
+</template>
+
+<style scoped></style>
